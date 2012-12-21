30b457296e34f7f8075a241bdde89a76aa4ac559
Ted Lilley <ted.lilley@gmail.com>
2012-12-21 04:18:42 -0500

    Remove go references, change rvm to user, remove .zshrc as sentinel

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,7 @@
 include_recipe "build-essential"
 include_recipe "git"
-include_recipe "golang"
 include_recipe "rvm::vagrant"
-include_recipe "rvm::system"
+include_recipe "rvm::user"
 package "zsh"
 
 bash "Install ZSH" do
@@ -11,10 +10,8 @@ bash "Install ZSH" do
     su vagrant
     git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
     cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-    sed -i 's/ZSH_THEME\=.*/ZSH_THEME\=\"fino-time\"/g' /home/vagrant/.zshrc
+    sed -i 's/ZSH_THEME\=.*/ZSH_THEME\=\"kolo\"/g' /home/vagrant/.zshrc
     sed -i '1i\\ source /etc/profile' /home/vagrant/.zshrc
-    echo 'source /home/vagrant/.bash_golang' >> /home/vagrant/.zshrc
     sudo chsh -s /bin/zsh vagrant
   EOC
-  creates "/home/vagrant/.zshrc"
 end
