ad50f033a7d6ffdeb5d79cec256a262b2443fb62
Hongchao Liu <lbs.lhc@gmail.com>
2014-02-16 18:00:01 +0100

    use ubuntu as default name

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,13 +5,13 @@ include_recipe "rvm::user"
 package "zsh"
 
 bash "Install ZSH" do
-  cwd "/home/vagrant"
+  cwd "/home/ubuntu"
   code <<-EOC
-    su vagrant
-    git clone git://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh
-    cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc
-    sed -i 's/ZSH_THEME\=.*/ZSH_THEME\=\"kolo\"/g' /home/vagrant/.zshrc
-    sed -i '1i\\ source /etc/profile' /home/vagrant/.zshrc
-    sudo chsh -s /bin/zsh vagrant
+    su ubuntu
+    git clone git://github.com/robbyrussell/oh-my-zsh.git /home/ubuntu/.oh-my-zsh
+    cp /home/ubuntu/.oh-my-zsh/templates/zshrc.zsh-template /home/ubuntu/.zshrc
+    sed -i 's/ZSH_THEME\=.*/ZSH_THEME\=\"kolo\"/g' /home/ubuntu/.zshrc
+    sed -i '1i\\ source /etc/profile' /home/ubuntu/.zshrc
+    sudo chsh -s /bin/zsh ubuntu
   EOC
 end
